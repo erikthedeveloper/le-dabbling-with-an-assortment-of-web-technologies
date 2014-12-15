10763a720b5c1a7b8993e2b037486f5d64e56a23
Erik Aybar <eaybar@titanium.cs.dixie.edu>
2014-12-15 11:50:37 -0700

    Adds CIT Credentials w/ Placeholder. Confirmed working on CIT network. Yay!

--- php/database.php
+++ php/database.php
@@ -1,18 +1,29 @@
 <?php
 
-$credentials = [
+$my_credentials = [
   // TODO: CIT CREDENTIALS HERE!
   // mysql -h127.0.0.1 -uhomestead -P33060 -psecret
   "host"     => "127.0.0.1",
   "username" => "homestead",
   "password" => "secret",
-  "database" => "cs4000_fun"
+  "database" => "cs4000_fun",
+  "port" => 33060
 ];
+
+$cit_credentials = [
+    "host"     => "mysql.cs.dixie.edu",
+    "username" => "eaybar",
+    "password" => "MYBIGSECRET!!!!",
+    "database" => "eaybar",
+    "port" => 3306
+  ];
+
+  $credentials = $cit_credentials;
 
 $DB = new \App\MyPdo(
   $credentials["host"],
   $credentials["username"],
   $credentials["password"],
   $credentials["database"],
-  33060
+  $credentials["port"]
 );
