ced3dca81fba5e3402d6adf00f26285765154d5d
Erik Aybar <eaybar@vanadium.cs.dixie.edu>
2014-12-18 16:29:13 -0700

    Adds dummy image into php/uploads. gitkeep

---
+++ php/uploads/index.php
@@ -0,0 +1,12 @@
+<?php require_once 'bootstrap.php' ?>
+<?= partial("header") ?>
+
+    <div class="container">
+        <h1>Some Uploads Here!</h1>
+
+        <div class="jumbotron">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab possimus, a veniam.</p>
+        </div>
+    </div>
+
+<?= partial("footer") ?>
