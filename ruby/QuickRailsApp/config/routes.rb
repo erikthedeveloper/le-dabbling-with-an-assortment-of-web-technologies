fb7d9276af0a9fd04ac730579331a6cdcf7aede8
Erik Aybar <eaybar@vanadium.cs.dixie.edu>
2014-12-18 16:42:01 -0700

    Adds pages_controller and #home

--- ruby/QuickRailsApp/config/routes.rb
+++ ruby/QuickRailsApp/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'pages/home'
+
+  get 'pages/foo'
+
+    root "pages#home"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
